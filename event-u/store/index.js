0211d823d8501a460027c96b79ad3368fbe281dc
IrfDev <irving.dev@icloud.om>
2020-03-13 17:00:06 -0600

    Creating the Vuex modules for Quotations,Events,Services and Needs

--- event-u/store/index.js
+++ event-u/store/index.js
@@ -1,14 +1,21 @@
 import Vuex from 'vuex'
 import events from './modules/events'
+import quotations from './modules/quotations'
+import services from './modules/services'
+import needs from './modules/needs'
 
 const createStore = () => {
     return new Vuex.Store({
         modules: {
             event: events,
+            quotation: quotations,
+            service: services,
+            need: needs
         },
         state: {
             activePage: '',
             lastPage: '',
+            activeUser: 'Jejejeje'
         },
         mutations: {
             change: (state, name) => {
